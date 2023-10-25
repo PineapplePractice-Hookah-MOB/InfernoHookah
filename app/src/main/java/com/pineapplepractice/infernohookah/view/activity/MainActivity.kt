9341408ae049d83c9037a6a3a4d8623b2bbfc35c
apdog <alex.puhow@gmail.com>
2023-10-25 20:50:23 +0300

    add navigation

--- app/src/main/java/com/pineapplepractice/infernohookah/view/activity/MainActivity.kt
+++ app/src/main/java/com/pineapplepractice/infernohookah/view/activity/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.pineapplepractice.infernohookah.view.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.pineapplepractice.infernohookah.R
 import com.pineapplepractice.infernohookah.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +17,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initBottomNavigationMenu()
+    }
+
+    private fun initBottomNavigationMenu() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_menu_navigation)
+        val navController = findNavController(R.id.fragment_placeholder)
+        bottomNavigationView.setupWithNavController(navController)
+        bottomNavigationView.setOnItemReselectedListener { item ->
+            val reselectedDestinationId = item.itemId
+            navController.popBackStack(reselectedDestinationId, inclusive = false)
+        }
     }
 }
