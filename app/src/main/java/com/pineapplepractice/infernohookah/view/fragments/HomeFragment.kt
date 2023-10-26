6a97ec830c330b806c70ce3bac91682086a916eb
apdog <alex.puhow@gmail.com>
2023-10-26 23:39:00 +0300

    improve auth layout

--- app/src/main/java/com/pineapplepractice/infernohookah/view/fragments/HomeFragment.kt
+++ app/src/main/java/com/pineapplepractice/infernohookah/view/fragments/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.viewModels
 import com.pineapplepractice.infernohookah.databinding.FragmentHomeBinding
 import com.pineapplepractice.infernohookah.viewmodel.HomeViewModel
@@ -24,5 +25,8 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
     }
+
 }
