18f85d02b2dd178cba3f7705b3382a785116e878
maevskiEd <maevski.ed@gmail.com>
2023-11-08 18:47:29 +0300

    подправил фрагмент бронь - поменял элементы, подправил фон для кнопок. реализовал выбор даты и времени и выбор количества гостей из выпадающего меню. Комментарии тоже вводятся.

--- app/src/main/java/com/pineapplepractice/infernohookah/view/fragments/ReservationFragment.kt
+++ app/src/main/java/com/pineapplepractice/infernohookah/view/fragments/ReservationFragment.kt
@@ -1,14 +1,19 @@
 package com.pineapplepractice.infernohookah.view.fragments
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.viewModels
+import com.pineapplepractice.infernohookah.R
 import com.pineapplepractice.infernohookah.databinding.FragmentReservationBinding
+import com.pineapplepractice.infernohookah.utils.DateTimePicker
 import com.pineapplepractice.infernohookah.viewmodel.ReservationViewModel
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class ReservationFragment : Fragment() {
     private var _binding: FragmentReservationBinding? = null
@@ -25,5 +30,19 @@ class ReservationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val adapter = ArrayAdapter.createFromResource(requireContext(), R.array.guests,R.layout.spinner_layout)
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_layout)
+        binding.spinner.adapter = adapter
+
+        binding.btDateAndTime.setOnClickListener {
+            DateTimePicker(requireContext()) { dateTimeInMillis ->
+                // Делайте необходимые действия с dateTimeInMillis (время в миллисекундах эпохи)
+                println("Время в милисекундках эпохи: $dateTimeInMillis ")
+                val format = SimpleDateFormat("dd/MM/yyyy HH:mm", Locale("ru", "RU"))
+                println("Дата в удобном виде: ${format.format(Date(dateTimeInMillis))}")
+                binding.btDateAndTime.text = format.format(Date(dateTimeInMillis))
+            }
+        }
     }
 }
