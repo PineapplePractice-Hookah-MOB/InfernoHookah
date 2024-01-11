5b4ab4acbaf24bc5f9dc5621b1202367d9d51a6a
maevskiEd <maevski.ed@gmail.com>
2024-01-11 20:27:42 +0300

    11/01/2024 поменял DishesRecyclerAdapter

--- app/src/main/java/com/pineapplepractice/infernohookah/view/rvadapters/DishesRecyclerAdapter.kt
+++ app/src/main/java/com/pineapplepractice/infernohookah/view/rvadapters/DishesRecyclerAdapter.kt
@@ -7,14 +7,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.pineapplepractice.infernohookah.R
 import com.pineapplepractice.infernohookah.data.Dishes
+import com.pineapplepractice.infernohookah.data.Tea
 import com.pineapplepractice.infernohookah.data.listOfDishes
 import com.pineapplepractice.infernohookah.databinding.DishesItemBinding
 
 class DishesRecyclerAdapter(private var items: List<Dishes>) :
     RecyclerView.Adapter<DishesRecyclerAdapter.InnerDishesViewHolder>() {
 
-    private var _binding: DishesItemBinding? = null
-    private val binding get() = _binding!!
+    /*    private var _binding: DishesItemBinding? = null
+        private val binding get() = _binding!!*/
 
     //    private var items = listOfTea
 //    private var returnItems = listOfTea
@@ -23,21 +24,53 @@ class DishesRecyclerAdapter(private var items: List<Dishes>) :
     /*    private var items = listOfDishes
         private var returnItems = listOfDishes*/
 
-    inner class InnerDishesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    //    inner class InnerDishesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    inner class InnerDishesViewHolder(binding: DishesItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val nameOfDish = binding.nameOfDish
+        val descriptionOfDish = binding.descriptionOfDish
+        val countOfDish = binding.countOfDish
+        val priceOfDish = binding.priceOfDish
+        val dishesCardView = binding.dishesCardView
+        val imageOfDish = binding.imageOfDish
+    }
 
     override fun getItemCount() = items.size
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): InnerDishesViewHolder {
-        _binding = DishesItemBinding.inflate(
+    private fun setItems(newItems: List<Dishes>) {
+        items = newItems
+        notifyDataSetChanged()
+    }
+
+    /*    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): InnerDishesViewHolder {
+            *//*        _binding = DishesItemBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false
+                )*//*
+        val binding = DishesItemBinding.inflate(
             LayoutInflater.from(parent.context), parent, false
         )
         return InnerDishesViewHolder(binding.root)
+//        return InnerDishesViewHolder(binding.root)
+    } */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): InnerDishesViewHolder {
+        return InnerDishesViewHolder(DishesItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
     override fun onBindViewHolder(holder: InnerDishesViewHolder, position: Int) {
         val tea = items[position]
 
-        holder.itemView.apply {
+        holder.nameOfDish.text = tea.name
+        holder.descriptionOfDish.text = tea.description
+        holder.countOfDish.text = tea.count.toString() + "мл"
+        holder.priceOfDish.text = tea.price.toString() + "р."
+
+        Glide.with(holder.dishesCardView)
+            .load(tea.image)
+            .error(R.drawable.ic_menu)
+            .centerCrop()
+            .into(holder.imageOfDish)
+
+/*        holder.itemView.apply {
             binding.nameOfDish.text = tea.name
             binding.descriptionOfDish.text = tea.description
             binding.countOfDish.text = tea.count.toString() + "мл"
@@ -48,7 +81,7 @@ class DishesRecyclerAdapter(private var items: List<Dishes>) :
                 .error(R.drawable.ic_menu)
                 .centerCrop()
                 .into(binding.imageOfDish)
-        }
+        }*/
     }
 
     /*    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
